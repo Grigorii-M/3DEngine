dd87d276a5a2bfa6b8dade2caca442e833fcc3c7
Grigorii-M <grigory36m@gmail.com>
2022-05-06 23:08:26 +0300

    Added drawing of an object in orthographic projection using simple drawing algorithm

--- src/Vertex.java
+++ src/Vertex.java
@@ -2,5 +2,25 @@
  * Coordinates of a vertex in a right-handed coordinate system,
  * with positive z pointing towards the viewer
  */
-public record Vertex(double x, double y, double z) {
+public class Vertex {
+
+    public double x, y, z;
+
+    public Vertex(double x, double y, double z) {
+        this.x = 100 * x;
+        this.y = 100 * y;
+        this.z = 100 * z;
+    }
+
+    public double x() {
+        return x;
+    }
+
+    public double y() {
+        return y;
+    }
+
+    public double z() {
+        return z;
+    }
 }
